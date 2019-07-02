c74fa61de5a0cdea756b3be0465209923f3186b9
XiaofengZhu <xiaofengzhu2013@u.northwestern.edu>
2019-07-02 14:43:55 -0500

    temp changes

--- src/model/modeling.py
+++ src/model/modeling.py
@@ -194,7 +194,7 @@ def build_residual_model(mode, inputs, params, weak_learner_id):
     # # residuals = tf.Print(residuals, [residuals], message='residuals\n')
     # # residual_predicted_scores = tf.Print(residual_predicted_scores, [residual_predicted_scores], message='residual_predicted_scores\n')
     # mse_loss = tf.losses.mean_squared_error(residuals, residual_predicted_scores)
-    return inputs['old_predicted_scores']+2*inputs['residuals'], mse_loss
+    return inputs['old_predicted_scores']+3*inputs['residuals'], mse_loss
 
 # new weights for fc1_drop
 # def build_residual_model(is_training, inputs, params, weak_learner_id):

65e4060354b82b7101cf5cad498fd2cc064817d2
XiaofengZhu <xiaofengzhu2013@u.northwestern.edu>
2019-07-06 17:56:10 -0500

    roll back

--- src/model/reader.py
+++ src/model/reader.py
@@ -8,6 +8,7 @@ import argparse
 import logging
 
 from model.utils import Params
+from model.modeling import retrain_lenet
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--model_dir', default='experiments/base_model',
@@ -67,7 +68,18 @@ def input_fn(mode, dataset, params):
     features = tf.reshape(features, [-1, \
         height, width, depth])
     # labels = tf.reshape(labels, [-1, 1])
-    labels = tf.one_hot(labels, params.num_classes) 
+    labels = tf.one_hot(labels, params.num_classes)
+    if params.loss_fn == 'boost':
+        predicted_scores, _ = retrain_lenet(features, params, var_scope='c_cnn')
+        predicted_scores = tf.stop_gradients(predicted_scores)
+        # iterator_init_op = iterator.initializer
+        inputs = {
+            # 'iterator_init_op': iterator_init_op,
+            'features': features,
+            'labels': labels,
+            'old_predicted_scores': predicted_scores,
+            }
+        return inputs       
     # iterator_init_op = iterator.initializer
     inputs = {
         # 'iterator_init_op': iterator_init_op,

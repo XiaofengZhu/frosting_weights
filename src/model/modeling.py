4b76021c117bf9f3358d2a030eab6bfa8191c8de
XiaofengZhu <xiaofengzhu2013@u.northwestern.edu>
2019-07-08 12:33:13 -0500

    debugging

--- src/model/modeling.py
+++ src/model/modeling.py
@@ -15,12 +15,12 @@ import time
 def lenet2(X, params=None, var_scope='cnn2'):
     with tf.variable_scope(var_scope, reuse=tf.AUTO_REUSE):
         # CONVOLUTION 1 - 1
-        with tf.name_scope('conv1_1'):
-            filter1_1 = tf.get_variable('weights1_1', shape=[5, 5, int(params.depth), 2], \
+        with tf.name_scope('conv1'):
+            filter1_1 = tf.get_variable('weights1', shape=[5, 5, int(params.depth), 2], \
                 initializer=tf.truncated_normal_initializer(stddev=1e-1))
             stride = [1,1,1,1]
             conv = tf.nn.conv2d(X, filter1_1, stride, padding='SAME')
-            biases = tf.get_variable('biases1_1', shape=[2], \
+            biases = tf.get_variable('biases1', shape=[2], \
                 initializer=tf.constant_initializer(0.0))
             out = tf.nn.bias_add(conv, biases)
             conv1_1 = tf.nn.relu(out)
@@ -30,14 +30,14 @@ def lenet2(X, params=None, var_scope='cnn2'):
                                      ksize=[1, 2, 2, 1],
                                      strides=[1, 2, 2, 1],
                                      padding='SAME',
-                                     name='pool1_1')
+                                     name='pool1')
             pool1_1_drop = tf.nn.dropout(pool1_1, params.training_keep_prob)
         # CONVOLUTION 1 - 2
-        with tf.name_scope('conv1_2'):
-            filter1_2 = tf.get_variable('weights1_2', shape=[5, 5, 2, 2], \
+        with tf.name_scope('conv2'):
+            filter1_2 = tf.get_variable('weights2', shape=[5, 5, 2, 2], \
                 initializer=tf.truncated_normal_initializer(stddev=1e-1))
             conv = tf.nn.conv2d(pool1_1_drop, filter1_2, [1,1,1,1], padding='SAME')
-            biases = tf.get_variable('biases1_2', shape=[2], \
+            biases = tf.get_variable('biases2', shape=[2], \
                 initializer=tf.constant_initializer(0.0))
             out = tf.nn.bias_add(conv, biases)
             conv1_2 = tf.nn.relu(out)
@@ -47,26 +47,36 @@ def lenet2(X, params=None, var_scope='cnn2'):
                                      ksize=[1, 2, 2, 1],
                                      strides=[1, 2, 2, 1],
                                      padding='SAME',
-                                     name='pool2_1')
+                                     name='pool2')
             pool2_1_drop = tf.nn.dropout(pool2_1, params.training_keep_prob)
-        #FULLY CONNECTED 1
-        with tf.name_scope('fc1') as scope:
-            pool2_flat = tf.layers.Flatten()(pool2_1_drop)
-            dim = pool2_flat.get_shape()[1].value
-            fc1w = tf.get_variable('weights3_1', shape=[dim, 1024], \
+        # #FULLY CONNECTED 1
+        # with tf.name_scope('fc1') as scope:
+        #     pool2_flat = tf.layers.Flatten()(pool2_1_drop)
+        #     dim = pool2_flat.get_shape()[1].value
+        #     fc1w = tf.get_variable('weights3_1', shape=[dim, 1024], \
+        #         initializer=tf.truncated_normal_initializer(stddev=1e-1))
+        #     fc1b = tf.get_variable('biases3_1', shape=[1024], \
+        #         initializer=tf.constant_initializer(1.0))
+        #     out = tf.nn.bias_add(tf.matmul(pool2_flat, fc1w), fc1b)
+        #     fc1 = tf.nn.relu(out)
+        #     fc1_drop = tf.nn.dropout(fc1, params.training_keep_prob)
+        # #FULLY CONNECTED 2
+        # with tf.name_scope('fc2') as scope:
+        #     fc2w = tf.get_variable('weights3_2', shape=[1024, params.num_classes], \
+        #         initializer=tf.truncated_normal_initializer(stddev=1e-1))
+        #     fc2b = tf.get_variable('biases3_2', shape=[params.num_classes], \
+        #         initializer=tf.constant_initializer(1.0))
+        #     Ylogits = tf.nn.bias_add(tf.matmul(fc1_drop, fc2w), fc2b)
+        with tf.name_scope('conv3'):
+            W_conv3 = tf.get_variable('weights3', shape=[1, 1, 2, params.num_classes], \
                 initializer=tf.truncated_normal_initializer(stddev=1e-1))
-            fc1b = tf.get_variable('biases3_1', shape=[1024], \
+            b_conv3 = tf.get_variable('biases3', shape=[params.num_classes], \
                 initializer=tf.constant_initializer(1.0))
-            out = tf.nn.bias_add(tf.matmul(pool2_flat, fc1w), fc1b)
-            fc1 = tf.nn.relu(out)
-            fc1_drop = tf.nn.dropout(fc1, params.training_keep_prob)
-        #FULLY CONNECTED 2
-        with tf.name_scope('fc2') as scope:
-            fc2w = tf.get_variable('weights3_2', shape=[1024, params.num_classes], \
-                initializer=tf.truncated_normal_initializer(stddev=1e-1))
-            fc2b = tf.get_variable('biases3_2', shape=[params.num_classes], \
-                initializer=tf.constant_initializer(1.0))
-            Ylogits = tf.nn.bias_add(tf.matmul(fc1_drop, fc2w), fc2b)
+            h_conv3 = tf.nn.relu(conv2d(pool2_1_drop, W_conv3) + b_conv3)      
+        with tf.name_scope('sum'):
+            batch_size, pool_output_h, pool_output_w, filter_n = [d.value for d in h_conv3.get_shape()]
+            h_conv3_flat = tf.reshape(h_conv3, [-1, pool_output_h * pool_output_w, filter_n])
+            Ylogits = tf.reduce_sum(h_conv3_flat, reduction_indices=1)            
     return Ylogits, fc1_drop
 
 def lenet(X, params=None, var_scope='cnn'):

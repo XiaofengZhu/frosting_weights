2685fe5e6a90dcbf2793be8fec79273478e059a4
XiaofengZhu <xiaofengzhu2013@u.northwestern.edu>
2019-07-08 23:37:08 -0500

    updated regu

--- src/model/modeling.py
+++ src/model/modeling.py
@@ -458,7 +458,8 @@ def model_fn(mode, inputs, params, reuse=False, weak_learner_id=0):
                     # Register layers
                     layer_collection.auto_register_layers()
                     # Construct training ops
-                    optimizer = kfac.PeriodicInvCovUpdateKfacOpt(params.learning_rate, layer_collection=layer_collection)
+                    optimizer = kfac.PeriodicInvCovUpdateKfacOpt(params.learning_rate, damping=0.001, \
+                        layer_collection=layer_collection)
                     train_op = optimizer.minimize(loss, global_step=global_step)         
             else:
                 with tf.name_scope('adam_optimizer'):

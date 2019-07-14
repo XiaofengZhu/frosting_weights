9e4d1bedcd6bb0ecc4e11e629a7105260439736f
XiaofengZhu <xiaofengzhu2013@u.northwestern.edu>
2019-07-13 20:36:42 -0500

    run*.py

--- src/run_mas.py
+++ src/run_mas.py
@@ -3,14 +3,14 @@ import os
 model_dir = 'experiments/base_model'
 train_log = os.path.join(model_dir, 'train.log')
 test_log = os.path.join(model_dir, 'test.log')
-for i in range(8, 11):
+for i in range(6, 11):
 	train_script = 'CUDA_VISIBLE_DEVICES=1 python main.py --train_range [1-{}] \
-	--loss_fn cnn'.format(i)
+	 --loss_fn retrain_regu_mas --finetune true'.format(i)
 	os.system('echo {} >> {}'.format(train_script, train_log))
 	os.system(train_script)
 	test_fake_script = 'python evaluate.py --train_range [1-{}] \
-	--loss_fn cnn --use_kfac true --finetune true'.format(i)
+	 --loss_fn retrain_regu_mas --finetune true'.format(i)
 	os.system('echo {} >> {}'.format(test_fake_script, test_log))
 	test_script = 'CUDA_VISIBLE_DEVICES=1 python evaluate.py \
-	--loss_fn cnn --finetune true'.format(i)	
+	 --loss_fn retrain_regu_mas --finetune true'.format(i)	
 	os.system(test_script)

60b4ee01896938dfcd948bae5df54c40ba934bcf
XiaofengZhu <xiaofengzhu2013@u.northwestern.edu>
2019-07-18 12:17:34 -0500

    updated boost

--- src/model/modeling.py
+++ src/model/modeling.py
@@ -595,7 +595,7 @@ def build_residual_model(mode, inputs, params, weak_learner_id):
     #     message='residual_predicted_scores\n')
     boosted_scores = inputs['old_predicted_scores'] + residual_predicted_scores
     return boosted_scores, mse_loss
-'''
+
 def get_residual(labels, Ylogits):
     Ysoftmax = tf.nn.softmax(Ylogits)
     return labels - Ysoftmax

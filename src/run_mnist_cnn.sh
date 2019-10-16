12d10cac14bc4e024216c574e3ed2bd69a7a7cf0
XiaofengZhu <xiaofengzhu2013@u.northwestern.edu>
2019-10-16 13:51:28 -0700

    run

--- src/run_mnist_cnn.sh
+++ src/run_mnist_cnn.sh
@@ -1,6 +1,6 @@
 ./bash.sh
-CUDA_VISIBLE_DEVICES=1 nohup python main.py --loss_fn cnn --data_dir ${TF_IMAGE_DATA}/mnist-inc
-CUDA_VISIBLE_DEVICES=1 nohup python evaluate.py --loss_fn cnn --data_dir ${TF_IMAGE_DATA}/mnist-inc >> mnist-inc.txt
+CUDA_VISIBLE_DEVICES=1 nohup python main.py --loss_fn cnn --data_dir ../../mnist-inc
+CUDA_VISIBLE_DEVICES=1 nohup python evaluate.py --loss_fn cnn --data_dir ../../mnist-inc >> mnist-inc.txt
 
 echo "\n" >> mnist-inc.txt
 cat experiments/base_model/params.json >> mnist-inc.txt

dcfae1e3d46a14979120c60a0ee73d986076c901
XiaofengZhu <xiaofengzhu2013@u.northwestern.edu>
2019-10-16 10:56:12 -0700

    boost

--- src/model/modeling.py
+++ src/model/modeling.py
@@ -599,7 +599,6 @@ def build_residual_model_with_fisher(mode, inputs, params, weak_learner_id):
     return boosted_scores, None
 
 '''
-'''
 def build_residual_model(mode, inputs, params, weak_learner_id):
     """Compute logits of the model (output distribution)
     Args:
@@ -617,8 +616,8 @@ def build_residual_model(mode, inputs, params, weak_learner_id):
     features = inputs['features']
     boosted_scores, _ = lenet_boost(features, is_training, params, var_scope='cnn')
     return boosted_scores, None
-'''
 
+'''
 def build_residual_model(mode, inputs, params, weak_learner_id):
     """Compute logits of the model (output distribution)
     Args:
@@ -657,7 +656,7 @@ def build_residual_model(mode, inputs, params, weak_learner_id):
     # # #     message='residual_predicted_scores\n')
     # boosted_scores = inputs['old_predicted_scores'] + residual_predicted_scores
     return boosted_scores, mse_loss
-
+'''
 
 '''
 def build_residual_model(mode, inputs, params, weak_learner_id):
